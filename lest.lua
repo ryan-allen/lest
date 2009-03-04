d80fdf80e8d181f5a604bc997b2839267cf8008c
Ryan Allen <ryan@yeahnah.org>
2009-03-04 22:29:14 +1100

    holy crap it works

--- lest.lua
+++ lest.lua
@@ -7,16 +7,17 @@ suite = {
   -- whatever the function returns is passed in as an arg
   -- for each test method as the fixture
   setup = function()
+    print('setting up coz i said so') -- debug yay
     return 'a string'
   end,
   -- and, yeah, clean up the fixture if required, too
   teardown = function(fixture)
-    -- do any clean up on fixture
+    print('burning down, hot topic' .. fixture) -- debug yay
   end,
   tests = {
     {
       description = 'it is a string',
-      run = function(fixture)
+      test = function(fixture)
         -- return true or false, false indicates a failure
         -- and true indicates a pass, nice!
         -- i know this is limited, but i'll add more complicated
@@ -28,13 +29,13 @@ suite = {
     },
     {
       description = 'it has a value of: a string',
-      run = function(fixture)
+      test = function(fixture)
         return fixture == 'a string'
       end
     },
     {
       description = 'this fails, hehe',
-      run = function(fixture)
+      test = function(fixture)
         return false -- oh how i miss ruby's implicit return :)
       end
     }
@@ -52,3 +53,59 @@ suite = {
 -- test 'testing the rest runner': .....!.!!.  done.
 --  - 'test that it is a string' FAILED
 -- yeah yeah i know, it's not very helpful, we'll get there
+
+-- questions that need answering:
+-- can i return multiple args and pass em in to the test functions?
+-- should i use assert instead?
+-- what happens when there is no setup/teardown?
+-- what happens when there is?
+-- should i verify the tables are marked up correctly?
+
+runner = {
+  run = function(suite)
+    print('Suite: ' .. suite.description)
+    for i, test in pairs(suite.tests) do
+      print('  Test: ' .. test.description)
+      -- set the fixture if there is a setup for the suite
+      if suite.setup then fixture = suite.setup() end
+      -- if a function has no args, and you pass em
+      -- in, it just ignores the args, is all, and
+      -- if you call a var or wtf it is, and it is
+      -- not set, it'll just return nil
+      -- so, call the test, and if it passes, yay, or not
+      if test.test(fixture) then
+        print '    PASS'
+      else
+        print '    FAIL'
+      end
+      -- if there is a setup and a teardown, then teardown the fixture
+      if suite.setup and suite.teardown then suite.teardown(fixture) end
+    end
+    print 'Done.'
+  end
+}
+
+runner.run(suite)
+
+-- more more more !!!
+
+runner.run({
+  description = 'Concatenating strings.',
+  setup = function()
+    return 'Monster'
+  end,
+  tests = {
+    {
+      description = 'Adding handbag to the fixture.',
+      test = function(fixture)
+        return (fixture .. ' Handbag') == 'Monster Handbag'
+      end
+    },
+    {
+      description = 'A failing test.',
+      test = function()
+        return false
+      end
+    }
+  }
+})
